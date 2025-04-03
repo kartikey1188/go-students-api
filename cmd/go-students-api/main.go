c719503e35a2fe5092f9ea748a0fc96a849b5380
kartikey1188 <kartikeysingh428@gmail.com>
2025-04-03 19:35:20 +0530

    added router and server

--- cmd/go-students-api/main.go
+++ cmd/go-students-api/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/kartikey1188/go-students-api/internal/config"
+)
 
 func main() {
-	fmt.Println("Welcome to students api")
+	// load config
+	cfg := config.MustLoad()
+	//database setup
+
+	//setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /",
+		func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("welcome to students api"))
+		})
+	//setup server
+
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	fmt.Printf("server started %s", cfg.Addr)
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("failed to start server")
+	}
 }
